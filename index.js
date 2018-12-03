446238a90e34183a567d3c3382a0950383677f24
Anders Hornor <anders.hornor@gmail.com>
2018-12-03 19:30:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (var i = 0; i < 4; i++) {
+    array.push(musicians[i]+" plays "+ instruments[i])
+  }
+  return array
+}
+
+function johnLennonFacts(facts) {
+ var fancyFacts = []
+ var counter = 0
+ while(counter < facts.length){
+   fancyFacts.push(facts[counter]+"!!!")
+   counter++
+ }
+ return fancyFacts
+}
+
+function iLoveTheBeatles(number) {
+  var array = [];
+  do {
+    array.push("I love the Beatles!")
+  }
+  while (array.length = number + 1)
+}
+
 // add solution here
