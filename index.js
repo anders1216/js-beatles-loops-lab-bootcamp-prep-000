e063107f9bf7879c0d0f199f6712bb8d7322d507
Anders Hornor <anders.hornor@gmail.com>
2018-12-03 18:19:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (var i = 0; i < 4; i++) {
+    array.push(musicians[i]+" plays "+ instruments[i])
+  }
+  return array
+}
+
+function johnLennonFacts(facts) {
+  var fancyFacts=[]
+  while (facts.length > 0){
+    fancyFacts.push(facts[1] + "!!!")
+  }
+  return fancyFacts
+}
+
 // add solution here
