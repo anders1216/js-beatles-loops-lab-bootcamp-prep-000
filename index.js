b8e5b93e3e80b67bdeaf2773f0653ea20589f8ed
Anders Hornor <anders.hornor@gmail.com>
2018-12-03 18:23:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (var i = 0; i < 4; i++) {
+    array.push(musicians[i]+" plays "+ instruments[i])
+  }
+  return array
+}
+
+function incrementVariable(){
+  i = i + 1
+}
+function johnLennonFacts(facts) {
+  var fancyFacts=[]
+  i = 0 
+  while (i < 5){
+    fancyFacts.push(facts[1] + "!!!")
+    incrementVariable()
+  }
+  return fancyFacts
+}
+
 // add solution here
